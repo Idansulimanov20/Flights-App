9373512c63335cf675dbfdb8b85a0c7421bc4fc5
Idansulimanov20 <idansolimanov22@gmail.com>
2025-02-09 09:46:51 +0200

    second lunch

--- front/src/components/Background.jsx
+++ front/src/components/Background.jsx
@@ -10,14 +10,12 @@ export default function Background() {
       <div className="cloud cloud3"></div>
       <div className="cloud cloud4"></div>
       <div className="cloud cloud5"></div>
-      <div className="plane">✈️</div>
-      <div className="plane">✈️</div>
-      <div className="plane">✈️</div>
       <div className="plane2">✈️</div>
       <div className="plane3">✈️</div>
       <div className="plane4">✈️</div>
       <div className="plane5">✈️</div>
       <div className="plane6">✈️</div>
+      <div className="plane4">✈️</div>
   
     </div>
   );
